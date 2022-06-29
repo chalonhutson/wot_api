1081b715dbf0674624f2df1298ccfbd5ced0b599
chalonhutson <chalonhutson@gmail.com>
2022-06-29 10:04:21 -0600

    Added app config vars in root app file.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from src import app
 from src.model import connect_to_db
 
 if __name__ == "__main__":
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ["POSTGRES_URI"]
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     connect_to_db(app)
     app.env = environ["DEV_ENVIRONMENT"]
     app.run(debug=False)
