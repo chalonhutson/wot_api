b3c7ae735d4ad42a2826d7059a2f13d16ae93856
chalonhutson <chalonhutson@gmail.com>
2022-06-29 09:46:21 -0600

    Updated everything seems to work.

--- src/forms.py
+++ src/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, validators, RadioField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional, AnyOf, Regexp
+from wtforms import BooleanField, StringField, validators, RadioField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional, AnyOf, Regexp, EqualTo
 
 class RegisterForm(FlaskForm):
     first_name = StringField("First Name",
-     validators=[Length(min=4, max=25)])
+     validators=[Length(min=4, max=25), DataRequired()])
     last_name = StringField("Last Name",
-    validators=[Length(min=4, max=25)])
+    validators=[Length(min=4, max=25), DataRequired()])
     discord = StringField("Discord Tag",
     validators=[Length(min=4, max=25)])
     submit = SubmitField("Submit")
